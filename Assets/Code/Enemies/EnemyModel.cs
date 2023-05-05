505806c5b6d6fecbde2665e6c92838bcfc697b8c
Gonzolo27 <gfontest@gmail.com>
2023-05-05 15:23:58 +0200

    enemy models wip

---
+++ Assets/Code/Enemies/EnemyModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyModel : CharacterModel
+{
+    /*
+     * TODO
+     * Imagino que habrá que almacenar aquí al player y ciertas cosas
+     */
+}
