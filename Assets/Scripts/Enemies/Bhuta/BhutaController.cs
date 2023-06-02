326fb98ef4ccde26dbaa6ef9a58a633552aa249d
Gonzolo27 <60710427+Gonzolo27@users.noreply.github.com>
2023-06-02 15:37:08 +0200

    litle changes

--- Assets/Scripts/Enemies/Bhuta/BhutaController.cs
+++ Assets/Scripts/Enemies/Bhuta/BhutaController.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class BhutaController : MonoBehaviour
+public class BhutaController : EnemyController<BhutaModel>
 {
-    BhutaModel model;
-
     void Start()
     {
         model = GetComponent<BhutaModel>();
---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Clase para métodos genéricos en cualquier Controlador de enemigo.
+ */
+public class EnemyController<T> : MonoBehaviour
+{
+    protected T model;
+}
