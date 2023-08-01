52fabf9f867b0c02de83a698fa39fbed2d2a6e63
Jennifercs07 <jenni_canuto_7@hotmail.com>
2023-08-01 20:46:53 +0200

    Cambio de escena añadido en algunas salas

--- Assets/Script/LevelGenerator.cs
+++ Assets/Script/LevelGenerator.cs
@@ -5,18 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class LevelGenerator : MonoBehaviour
 {
-
     private List<int> availableScenes;
-    private int numRooms = 14;
-    private int storeSceneIndex = 6;
-    private int bossSceneIndex = 13;
+    private int numRooms = 12;
+    private int storeSceneIndex = 13;
+    private int bossSceneIndex = 14;
+    private int countRooms = 0;
 
     private void Start()
     {
         //List to store the index of available scenes
         availableScenes = new List<int>();
 
-        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        for (int i = 1; i < numRooms; i++)
         {
             availableScenes.Add(i);
         }
@@ -25,27 +25,34 @@ public class LevelGenerator : MonoBehaviour
 
     public void ChangeToRandomScene()
     {
+        countRooms = 1;
         if (availableScenes.Count > 0)
         {
+            if (countRooms == 7)
+            {
+                SceneManager.LoadScene("Improvement1RoomLevel1");
+            }
+
+            if (countRooms == 13)
+            {
+                SceneManager.LoadScene("ObjectStore");
+            }
+
+            if (countRooms == 14)
+            {
+                SceneManager.LoadScene("HouseHall14Level1");
+            }
+
             //Get random index
-            int randomIndex = Random.Range(0, availableScenes.Count);
+            int randomIndex = Random.Range(1, availableScenes.Count);
             int sceneToLoad = availableScenes[randomIndex];
             availableScenes.RemoveAt(randomIndex);
-
-            if (sceneToLoad == storeSceneIndex || sceneToLoad == bossSceneIndex)
-            {
-                SceneManager.LoadScene(sceneToLoad);
-            }
-            else
-            {
-                // Cargamos la escena aleatoria
-                SceneManager.LoadScene(sceneToLoad);
-            }
+            SceneManager.LoadScene(sceneToLoad);
         }
         else
         {
             //If all scenes have been visited, we reset the list to play again.
-            for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+            for (int i = 1; i < numRooms; i++)
             {
                 availableScenes.Add(i);
             }
---
+++ Assets/Script/MovePlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private float horizontalMove;
+    private float verticalMove;
+    private Vector3 playerInput;
+    public CharacterController player;
+
+    [SerializeField] private float playerSpeed;
+
+    private Vector3 movePlayer;
+
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0.0f, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        movePlayer = playerInput * playerSpeed;
+
+        player.transform.position = player.transform.position + movePlayer;
+
+        player.Move(movePlayer * Time.deltaTime);
+    }
+}
