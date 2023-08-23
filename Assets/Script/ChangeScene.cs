23fc1d92a95385ccdd05280d09c08f601f2423d6
Jennifercs07 <jenni_canuto_7@hotmail.com>
2023-08-23 17:37:34 +0200

    Pruebas

--- Assets/Script/ChangeScene.cs
+++ Assets/Script/ChangeScene.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class ChangeScene : MonoBehaviour
 {
     [SerializeField] private LevelGenerator sceneLoader;
-    /*
+    
     private GameObject sceneLoaderObject;
-
+    
     private string sceneLoaderTag = "SceneLoader";
-
+    
     private void Start()
     {
+        //sceneLoader = GetComponentInParent<LevelGenerator>();
+        
         sceneLoaderObject = GameObject.FindGameObjectWithTag(sceneLoaderTag);
         if (sceneLoaderObject != null)
         {
+    
             sceneLoader = sceneLoaderObject.GetComponent<LevelGenerator>();
         }
         else
@@ -22,7 +25,7 @@ public class ChangeScene : MonoBehaviour
             Debug.LogError("No se encontro el GameObject con la etiqueta SceneController. Asegurate de que el GameObject SceneController esta presente en la escena.");
         }
     }
-    */
+    
 
     private void OnTriggerEnter(Collider other)
     {
