4ddac85f9893d54e745d7fbd277220ef62181367
Gonzolo27 <60710427+Gonzolo27@users.noreply.github.com>
2023-09-06 19:28:15 +0200

    ChangeScene added

---
+++ Assets/Scripts/Scenes/ChangeScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    //[SerializeField] private LevelGenerator sceneLoader;
+    
+    //private GameObject sceneLoaderObject;
+    
+    //Sprivate string sceneLoaderTag = "SceneLoader";
+    
+    private void Start()
+    {
+        //sceneLoader = GetComponentInChildren<LevelGenerator>();
+
+        /*sceneLoaderObject = GameObject.Find("SceneLoader");
+        if (sceneLoaderObject != null)
+        {
+
+            //sceneLoader = sceneLoaderObject.GetComponent<SceneManager>();
+        }
+        else
+        {
+            Debug.Log("No se encontro el GameObject con la etiqueta SceneController. Asegurate de que el GameObject SceneController esta presente en la escena.");
+        }*/
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Change to random scene
+            SceneManager.Instance.ChangeToRandomScene();
+            //sceneLoader.ChangeToRandomScene();
+        }
+    }
+}
