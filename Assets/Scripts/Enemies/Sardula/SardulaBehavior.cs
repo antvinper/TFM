481e889bd784417786ac45316428e28562bccb89
aritzquirce844 <117646385+aritzquirce844@users.noreply.github.com>
2023-12-03 10:42:04 +0100

    Satas y sardula comienzo

---
+++ Assets/Scripts/Enemies/Sardula/SardulaBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SardulaBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //IF not in range
+
+            //Dash
+            //Rayo
+
+        //ELSE
+
+            //IF Stun
+                //Slash
+
+            //ELSE
+                //IF Close pared
+                    //Al centro
+                    //Slash
+
+                //ELSE IF Not close pared
+                    //Slash
+                    //Stun
+                    //Dash
+
+        //Wait 4 sec
+    }
+}
