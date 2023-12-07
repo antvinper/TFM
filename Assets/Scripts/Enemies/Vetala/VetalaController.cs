3f1d623caabfc6ba61fbc96db7687a1efb63ab77
Jennifercs07 <jenni_canuto_7@hotmail.com>
2023-12-07 16:47:38 +0100

    PruebasEnemgisoJenny: Vetala 2.0

--- Assets/Scripts/Enemies/Vetala/VetalaController.cs
+++ Assets/Scripts/Enemies/Vetala/VetalaController.cs
@@ -11,44 +11,69 @@ public class VetalaController : EnemyController
 
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Animator animator;
+    [SerializeField] SkillDefinition skillPoison;
 
     private float walkSpeed;
 
-    private float latestChangeTime;
-    private readonly float changeTime = 3f;
     private Vector3 movementDir;
     private Vector3 speedDir;
     private float timeBetweenAttack = 2f;
     private float actualTimeBetweenAttack = 0;
-    private bool canAttack = true;
 
     void Start()
     {
+
         vetalaModel = new VetalaModel();
         this.SetModel(vetalaModel);
-
-        latestChangeTime = 0f;
 
         walkSpeed = GetStatValue(StatNames.SPEED, StatParts.ACTUAL_VALUE);
     }
 
     void Update()
     {
-        if (Time.time - latestChangeTime > changeTime)
-        {
-            latestChangeTime = Time.time;
-            calculateRandomVector();
-            animator.Play("Armature|Walk");
-        }
-
         rb.velocity = speedDir;
     }
 
-    void calculateRandomVector()
+    void calculateObjectiveVector(Vector3 objectivePos)
     {
         walkSpeed = GetStatValue(StatNames.SPEED, StatParts.ACTUAL_VALUE);
-        movementDir = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
+        movementDir = new Vector3(objectivePos.x - transform.position.x, 0, objectivePos.z - transform.position.z).normalized;
         transform.rotation = Quaternion.LookRotation(movementDir);
         speedDir = movementDir * walkSpeed;
+    }
+
+    public async Task ApplySkill(CompanyCharacterController target)
+    {
+        skillPoison.ProcessSkill(this, target);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Si el jugador entra en contacto con "Sphere Collider"
+        if (other.CompareTag("Player"))
+        {
+            //Realiza ataque en area "Aplastar" (Smash)
+            animator.Play("Armature|Attack");
+
+            //Moverse hacia el jugador
+            transform.LookAt(new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z));
+            calculateObjectiveVector(other.transform.position);
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Si el jugador entra en contacto con "Box Collider"
+        if (collision.collider.CompareTag("Player"))
+        {
+            PlayerController playerController = collision.collider.GetComponent<PlayerController>();
+            ApplySkill(playerController);
+        }
+        /*
+        if (collision.collider.CompareTag("Wall"))
+        {
+
+        }*/
     }
 }
