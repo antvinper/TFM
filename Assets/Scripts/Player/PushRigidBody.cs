1566b6239c3b00e4bfcbd30d0e16e32188d2a59f
Jennifercs07 <jenni_canuto_7@hotmail.com>
2024-01-13 10:21:49 +0100

    Arreglos con el rigidbody
    para que no pueda empujar el jugador a los enemigos ni subir a sus cabezas

---
+++ Assets/Scripts/Player/PushRigidBody.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushRigidBody : MonoBehaviour
+{
+    private float pushPower = 2.0f;
+    private float targetMass;
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //Almacena el rigidbody del objeto con el que esta colisionando el jugador
+        Rigidbody rb = hit.collider.attachedRigidbody;
+
+        if (rb == null || rb.isKinematic)
+        {
+            return;
+        } 
+        
+        //Si cae encima de un objeto no se hace nada
+        if (hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+
+        targetMass = rb.mass;
+
+        //La dirección hacia donde empuja
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        rb.velocity = pushDir * pushPower / targetMass;
+    }
+}
